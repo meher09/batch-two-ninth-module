ee57478dec751b0b7e4690e0e1b2a6d6bec8a11b
meher09 <mehernigarcu@gmail.com>
2022-12-01 18:11:25 +0600

    Change in Wordpress function file

--- wp_function.py
+++ wp_function.py
@@ -1,4 +1,5 @@
-import wp_func
+from wp_func import wp_p
+from wp_func import wp_h2
 
 
 first_paragraph_text = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
@@ -6,7 +7,7 @@ first_paragraph_text = "Contrary to popular belief, Lorem Ipsum is not simply ra
 heading_one_text = "Why do we use it?"
 second_paragraph_text = "It is a long established fact that a reader will be distracted by the readable content"
 
-article = wp_func.wp_p(first_paragraph_text)+wp_func.wp_h2(heading_one_text)+wp_func.wp_p(second_paragraph_text)
+article = wp_p(first_paragraph_text)+wp_h2(heading_one_text)+wp_p(second_paragraph_text)
 
 print(article)
 

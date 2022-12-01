73b809ac9bfa861a3042ed753c3a012a217610f1
meher09 <mehernigarcu@gmail.com>
2022-12-01 18:00:13 +0600

    first commit

---
+++ wp_function.py
@@ -0,0 +1,14 @@
+import wp_func
+
+
+first_paragraph_text = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
+
+heading_one_text = "Why do we use it?"
+second_paragraph_text = "It is a long established fact that a reader will be distracted by the readable content"
+
+article = wp_func.wp_p(first_paragraph_text)+wp_func.wp_h2(heading_one_text)+wp_func.wp_p(second_paragraph_text)
+
+print(article)
+
+
+
